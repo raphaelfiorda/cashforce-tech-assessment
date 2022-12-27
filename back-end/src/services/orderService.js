96fac2109326c31c55d10b075303f1b789a46f5f
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-12-27 04:04:05 -0300

    refactor:(orders) Adiciona os nomes dos sacados e cedentes na query

--- back-end/src/services/orderService.js
+++ back-end/src/services/orderService.js
@@ -2,7 +2,19 @@ const connection = require('../database/models');
 
 const userService = {
   list: async () => {
-    const orders = await connection.Order.findAll();
+    const orders = await connection.Order.findAll({
+      attributes: ['orderNumber', 'emissionDate', 'value', 'orderStatusBuyer'],
+      include: [{
+        model: connection.Buyer,
+        as: 'buyer',
+        attributes: ['name']
+      },
+      {
+        model: connection.Provider,
+        as: 'provider',
+        attributes: ['name']
+      }]
+    });
 
     return orders;
   },
